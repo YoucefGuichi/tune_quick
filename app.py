609f0f9d59cdc67ff9cf6c7bc58d5852eb4805bc
Youcef Guichi <youcef.guichi@pylink.co.uk>
2022-03-20 22:52:00 +0100

    -Add the parser to the index function -Finalize the branch F-02 -async code passed successfully and the branch F-02 is ready for merging

--- app.py
+++ app.py
@@ -1,53 +1,17 @@
-import io
-import logging as logger
+import asyncio
 
-import pandas as pd
 from flask import Flask, render_template, request
-from tensorflow.keras import Sequential
 
-from src.models import GRUModel, LSTMModel
-from src.parser import algorithms
+from src.parser import Parser
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST":
-        file = request.files["csv-file"]
-        algorithm = request.form["algorithm"]
-        loss_function = request.form["loss-function"]
-        epochs = request.form["epochs"]
-        batch_size = request.form["batch-size"]
-        optimizer = request.form["optimizer"]
-        chart_title = request.form["chart-title"]
-        x_axis_title = request.form["x-axis"]
-        y_axis_title = request.form["y-axis"]
-
-        # read csv file
-        try:
-            dataset = pd.read_csv(io.StringIO(file.stream.read().decode("UTF8"), newline=None))
-        except Exception as err:
-            raise err
-
-        seq = Sequential()
-        model = algorithms[algorithm](dataset, seq)
-
-        logger.info('cleaning ans preparing the data...')
-        try:
-            model.clean_and_prepare_dataset()
-            model.split_data()
-        except Exception as err:
-            raise err
-
-        logger.info('training started...')
-        model.train(optimizer=optimizer, loss=loss_function, epochs=int(epochs), batch_size=int(batch_size))
-        logger.info('training done')
-        model.predict()
-        chart = model.plot_predictions(chart_title, x_axis_title, y_axis_title)
-        return render_template("index.html", bar_chart=chart)
-    else:
-        return render_template("index.html")
+    parser = Parser(request=request)
+    asyncio.run(parser.main())
+    return render_template("index.html", bar_chart=parser.chart)
 
 
 if __name__ == "__main__":
--- src/parser.py
+++ src/parser.py
@@ -3,29 +3,27 @@ import io
 import logging as logger
 
 import pandas as pd
-from flask import render_template
-
-from src.models import LSTMModel, GRUModel
 from tensorflow.keras import Sequential
 
-# algorithms dict
-algorithms = {
-    'LSTM': LSTMModel,
-    'GRU': GRUModel
-}
+from src.models import LSTMModel, GRUModel
 
 
 class Parser:
 
     def __init__(self, request):
         self.request = request
+        self.chart = None
+        self.algorithmes = {
+            'LSTM': LSTMModel,
+            'GRU': GRUModel
+        }
 
     async def add_model_with_dataset_to_queue(self, queue):
         try:
-            dataset = await pd.read_csv(
-                io.StringIO(self.request.file["csv-file"].stream.read().decode("UTF8"), newline=None))
+            dataset = pd.read_csv(
+                io.StringIO(self.request.files["csv-file"].stream.read().decode("UTF8"), newline=None))
             seq = Sequential()
-            model = algorithms[self.request.form["algorithm"]](dataset, seq)
+            model = self.algorithms[self.request.form["algorithm"]](dataset, seq)
             queue.put_nowait(model)
         except Exception as err:
             raise err
@@ -49,16 +47,12 @@ class Parser:
                 batch_size=int(self.request.form["batch-size"]))
             logger.info('training done')
             model.predict()
-            chart = model.plot_predictions(self.request.form["chart-title"], self.request.form["x-axis"],
-                                           self.request.form["y-axis"])
-            return render_template("index.html", bar_chart=chart)
+            self.chart = model.plot_predictions(self.request.form["chart-title"], self.request.form["x-axis"],
+                                                self.request.form["y-axis"])
 
-        else:
-            return render_template("index.html")
+    async def main(self):
 
-    async def main(self, model):
-
-        queue = asyncio.Queue
+        queue = asyncio.Queue()
         await asyncio.wait([
             self.add_model_with_dataset_to_queue(queue),
             self.run_model(queue)]
